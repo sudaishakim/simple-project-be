d018bfa6f996ab5c2e0916207d19a70d0da88583
sudaishakim <dais@inspigo.id>
2022-02-02 10:00:47 +0700

    adding new features for backend

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const app = express();
 
 //routes
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/user.js";
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello to Memories API");
--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,15 @@ import express from "express";
 //controllers
 import postsController from "../controllers/postsController.js";
 
+// middleware
+import auth from "../middleware/auth.js";
+
 const router = express.Router();
 
 router.get("/", postsController.getPosts);
 router.post("/", postsController.createPost);
-router.patch("/:id", postsController.updatePost);
-router.delete("/:id", postsController.deletePost);
-router.patch("/:id/likePost", postsController.likePost);
+router.patch("/:id", auth, postsController.updatePost);
+router.delete("/:id", auth, postsController.deletePost);
+router.patch("/:id/likePost", auth, postsController.likePost);
 
 export default router;
